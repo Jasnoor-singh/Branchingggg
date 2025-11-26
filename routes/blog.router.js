87ec29a20b8a0217e40acb2d4cbc2d471f4bdbb4
Jasnoor <apple@Apples-MacBook-Pro-2.local>
2025-11-26 11:55:04 +0530

    blog  branch work

--- routes/blog.router.js
+++ routes/blog.router.js
@@ -3,6 +3,6 @@ const router=express.Router();
 router.get("/",(req,res)=>{
     res.send("Welcome to the Blog Home Page");
 });
-router.get("/posts",(req,res)=>{
-    res.send("List of blog posts");
+router.get("/mainnnnn",(req,res)=>{
+    res.send("List of blog posts hekoojoejojdf");
 });
